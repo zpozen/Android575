2f3f7044a894dd97d52e428609e6b92b046da1f2
zpozen <zpozen@uw.edu>
2012-05-30 19:38:35 -0700

    I think this is it - we are done!

--- src/megazi/smartup/SmartUpActivity.java
+++ src/megazi/smartup/SmartUpActivity.java
@@ -13,6 +13,8 @@ import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -32,6 +34,13 @@ import net.jeremybrooks.knicker.dto.Word;
 
 public class SmartUpActivity extends Activity implements OnTouchListener {
 
+	class TextPart {
+		int start = -1;
+		int end = 1;
+		String text = "";
+		int selWordIndex = -1;
+	}
+	
 	static private InputStream model = null;
 	static private PhraseFinder phraseHandler = null;
 
@@ -39,6 +48,7 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 	public void onCreate(Bundle savedInstanceState) {
 		
 		String reading = getIntent().getStringExtra("read");
+		this.setTitle(reading.substring(6)); //skip "texts/" prefix
 		
 		System.setProperty("WORDNIK_API_KEY", "d671ba7493f90deab796270fba309776b2f2395e2b412fae4");
 		TokenStatus status;
@@ -90,7 +100,7 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 
 		tv.setText(text);
 	}
-
+	
 	@Override
 	public boolean onTouch(View view, MotionEvent ev) {
 		
@@ -101,52 +111,64 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 			case MotionEvent.ACTION_UP:
 				long timediff = ev.getEventTime() - ev.getDownTime();
 				Log.v("INFO", "SmartUpActivity::onTouch ACTION_UP, timediff is " + timediff + " selection is " + getSelectedWord());
-				if (timediff >= ViewConfiguration.getLongPressTimeout()) {					
+				if (timediff >= ViewConfiguration.getLongPressTimeout()) {
+										
+					GetWordnikDefinitionTask wordnikTask = new GetWordnikDefinitionTask();
 					
-					String inputPhrase = getSelectedPhrase();
-					TextView tv = (TextView) findViewById(R.id.def); 					
-					GetWordnikDefinitionTask wordnikTask = new GetWordnikDefinitionTask();					
-					Phrase mwe = getMWE(inputPhrase, getAssets());	
-					if (mwe == null) {	
-						tv.setText(getSelectedWord().toUpperCase() + "\n\n");
-						wordnikTask.execute(new String[] { getSelectedWord() });
-					}
-					else {
-						String prepVerb =  mwe.getVerb() + " " + mwe.getPreposition();
-						Log.v("INFO", "Found MWE " + prepVerb);
-						tv.setText(prepVerb.toUpperCase() + "\n\n");
-						wordnikTask.execute(new String[] { prepVerb });
+					TextPart curSentence = getSelectedSentence();
+					if (curSentence != null) {
+						String inputSentence = curSentence.text;
+						
+						// find a phrasal verb in the current sentence:
+						Phrase mwe = extractPhrase(inputSentence);
+						if (mwe != null && mwe.getVerb().equals(getSelectedWord().text)) 
+						{								
+							String prepVerb =  mwe.getVerb() + " " + mwe.getPreposition();
+							Log.v("INFO", "Found MWE " + prepVerb);
+
+							// send the verb+prep to wordnik							
+							wordnikTask.execute(new String[] { prepVerb });
+						}
+						else 
+						{
+							// send just the selected word to wordnik
+							wordnikTask.execute(new String[] { getSelectedWord().text });
+						}
 					}
 				}
 				break;
-			}
+			}			
 			break;
 		}		
 		return false;
 	}
 
-	private String getSelectedWord()
+	private TextPart getSelectedWord()
 	{
+		TextPart ret = new TextPart();
 		EditText tv = (EditText) findViewById(R.id.edit1); 
-		int selectionStart = tv.getSelectionStart();
-		int selectionEnd = tv.getSelectionEnd();
+		ret.start = tv.getSelectionStart();
+		ret.end = tv.getSelectionEnd();
 
-		StringBuilder sb = new StringBuilder(selectionEnd - selectionStart + 1);
-		for (int i = selectionStart; i <= selectionEnd; i++)
+		StringBuilder sb = new StringBuilder(ret.end - ret.start + 1);
+		for (int i = ret.start; i < ret.end; i++)
 		{
 			sb.append(tv.getText().charAt(i));
 		}
-		return sb.toString();
+		ret.text = sb.toString();
+		return ret;
 	}
 	
-	private String getSelectedPhrase()
+	private TextPart getSelectedSentence()
 	{
+		TextPart ret = new TextPart();
+
 		// Just look for punctuation marks on either side of the selection.
 		EditText tv = (EditText) findViewById(R.id.edit1); 
 		int selectionStart = tv.getSelectionStart();
 		int selectionEnd = tv.getSelectionEnd();
 		if (selectionStart == selectionEnd) {
-			return "";
+			return null;
 		}
 		
 		CharSequence fullText = tv.getText();		
@@ -177,23 +199,29 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 			sb.append(tv.getText().charAt(i));
 		}
 		
-		Log.i("INFO", "selected phrase is " + sb.toString());
-		return sb.toString();
+		Log.i("INFO", "selected 'sentence' is " + sb.toString());
+		ret.start = selectionStart;
+		ret.end = selectionEnd;
+		ret.text = sb.toString();
+		return ret;
 	}
 
-	private Phrase getMWE(String inputPhrase, AssetManager assetMgr)
+	private Phrase extractPhrase(String inputPhrase)
 	{   
 		Phrase ret = null;
 		try {        	
 			if (model == null)	{
-				model = assetMgr.open("pos-en-general-brown.HiddenMarkovModel");
+				model = getAssets().open("pos-en-general-brown.HiddenMarkovModel");
 			}
 			if (phraseHandler == null) 	{				
 				phraseHandler = new PhraseFinder(model); // the phrase handler can't reach assets, so pass in a stream from here
 			}			
 			String [] indexed = inputPhrase.split("\\s+");
 			Phrase ph = phraseHandler.getRawPhrasalVerb(indexed);			
-			ret = (ph.ithinkIfoundSomething()) ? ph : null;			
+			if (ph.ithinkIfoundSomething()) {
+				ret = ph;
+				Log.i("extractPhrase", "Found phrase " + ph.getVerb() + " " + ph.getPreposition());
+			}
 			
 		} catch (IOException e) {
 			Log.e("ERROR", "Failed to find HMM file");
@@ -206,24 +234,50 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 	private class GetWordnikDefinitionTask extends AsyncTask<String, Void, String> {
 
 		@Override
-		protected String doInBackground(String... lemmas) {
+		protected String doInBackground(String... strs) {
 			
 			Log.i("Background", "Enter");
+			String[] surface_strs = strs[0].split("\\s+");
+			
+			boolean phrasal = (surface_strs.length == 2);
 
-			assert (lemmas.length == 1);			
+			// This will reconstruct the "verb prep":
+			StringBuilder sb = new StringBuilder(surface_strs[0]);
+			for (int i = 1; i < surface_strs.length; i++) {
+				sb.append(" " + surface_strs[i]);
+			}
 
 			StringBuilder defs = new StringBuilder();
-			try {				
-				Word wd = WordApi.lookup(lemmas[0], true, false);
-				defs.append(wd.getCanonicalForm() + "\n\n");
+			try {	
+				String lemma = getLemmaFromWordnik(surface_strs[0]);
+				Log.i("Background", "Found lemma '" + lemma + "' for " + surface_strs[0]); 
 				
-				List<Definition> def = WordApi.definitions(lemmas[0], 0, null, false, null, true, false);
+				defs.append(lemma.toUpperCase());
+				if (phrasal) {
+					defs.append(" " + surface_strs[1].toUpperCase());
+				}
+				defs.append("\n\n");
+				
+				List<Definition> def = WordApi.definitions(lemma, 0, null, false, null, true, false);
 				int i = 1;
 				for (Definition d : def) {
-					defs.append((i++) + ") " + d.getPartOfSpeech() + ": " + d.getText() + "\n");
+					if (!phrasal) 
+					{
+						defs.append((i++) + ") " + d.getPartOfSpeech() + ": " + d.getText() + "\n");
+					}
+					else if (d.getPartOfSpeech().equals("phrasal-verb"))
+					{
+						String origPrep = surface_strs[1].trim().toLowerCase();
+						String defPrep = d.getText().split("\\s+")[1].toLowerCase();						
+						Log.i("Background", "Orig prep is '" + origPrep + "' and def prep is '" + defPrep + "'"); 
+						
+						if (origPrep.equals(defPrep)) {						
+							defs.append((i++) + ") " + "phrasal-verb" + ": " + d.getText() + "\n");
+						}
+					}
 				}
 			} catch (KnickerException e) {
-				defs.append(e.getMessage());
+				// defs.append("Ummmm.... Wordnik's fault!\n");
 				e.printStackTrace();
 			}	
 			Log.i("Background", "Returning "+ defs);
@@ -235,7 +289,27 @@ public class SmartUpActivity extends Activity implements OnTouchListener {
 			//String[] senses = result.split("\n");
 
 			TextView tv = (TextView) findViewById(R.id.def); 
-			tv.append(result);
+			tv.setText(result);
+		}
+		
+		private String getLemmaFromWordnik (String original) {
+			
+			String lemma = original;
+			try {				
+				Word wd = WordApi.lookup(original, true, false);
+				String suggested = (wd.getSuggestions().size() > 0) ? wd.getSuggestions().get(0) : "";
+				String word = wd.getWord();
+				if (!suggested.isEmpty()) {
+					lemma = suggested;
+				}
+				else if (!word.isEmpty()) {
+					lemma = word;
+				}
+
+			} catch (KnickerException e) {				
+				Log.e("getLemmaFromWordnik", e.getMessage());
+			}
+			return lemma;			
 		}
 	}
 
